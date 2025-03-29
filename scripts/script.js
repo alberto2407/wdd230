962e083a5ebf34d713b55a879558ef4f89aa86ad
alberto2407 <albertofc2407@gmail.com>
2025-03-29 22:04:06 +0100

    Home Page Responsive Images

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,17 @@ const hamburgerElement = document.querySelector('#button');
 const navElement = document.querySelector('.links');
 const darkButton = document.querySelector('#dark-button');
 const mainElement = document.querySelector('main');
+const visitsDisplay = document.getElementById('visits');
+
+//Last Modified
+document.addEventListener("DOMContentLoaded", () => {
+    const currentYear = new Date().getFullYear();
+    const lastModified = document.lastModified;
+    const currentYearElement = document.getElementById('currentYear');
+    const lastModifiedElement = document.getElementById('lastModified');
+    currentYearElement.textContent = currentYear;
+    lastModifiedElement.textContent = `Last Modified: ${lastModified}`;
+});
 
 // Menu Hamburguer
 hamburgerElement.addEventListener('click', () => {
@@ -14,3 +25,16 @@ hamburgerElement.addEventListener('click', () => {
 darkButton.addEventListener('click', () => {
     mainElement.classList.toggle('dark');
 });
+
+// Number of Visits
+let numVisits = Number(window.localStorage.getItem('numVisits-ls'));
+
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+}
+else {
+    visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+localStorage.setItem('numVisits-ls', numVisits);
